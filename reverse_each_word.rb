c0bef33102d21672b7d98e417ce6a630ddda3ef2
Maayan Kfir <mayankfir@gmail.com>
2018-09-23 22:42:42 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(sentence)
+  new_sentence = sentence.split(" ")
+  reversed_sentence = new_sentence.each do |word|
+    word.reverse!
+  end
+  reversed_sentence.join(" ")
+end
+
+def reverse_each_word(sentence2)
+  sentence2.split(" ").collect {|word| word.reverse!}.join(" ")
+end
