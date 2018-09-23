02815c488cd3d8c87a398b24189cea95d5d7eb55
Maayan Kfir <mayankfir@gmail.com>
2018-09-23 22:25:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,11 @@ def reverse_each_word(sentence)
   end
   reversed_sentence.join(" ")
 end
+
+def reverse_each_word(sentence2)
+  sentence2.split(" ") = reversed_sentence2
+  reversed_sentence2 = new_sentence2.collect do |word|
+    word.reverse!
+  end
+  reversed_sentence2.join(" ")
+end
